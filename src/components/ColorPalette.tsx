b75d9906651d24e795d56295267361d3eca3c93c
robuGdada <paudelronish@gmail.com>
2024-05-15 21:21:45 +0545

    fix

--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -5,7 +5,6 @@ import {
 } from "@/store/colorPalette.store";
 
 const ColorPalette = () => {
-  
   const colors = Object.keys(COLOR_PALETTE) as ColorName[];
   return (
     <div className="color-palette">
